d7026775316d76230f54748fff22e48b95ecf469
Alx-137 <60942793+Alx-137@users.noreply.github.com>
2020-04-13 15:57:12 -0500

    Menu+Layout
    play layout should display correct on any device, horizontal or vertical. let me know if it doesn't

---
+++ app/src/main/java/com/example/memory_jogger/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.memory_jogger
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {        //When class is created, do this{
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)                  //show this layout
+
+        playMenuButton.setOnClickListener {             //when playMenuButton is clicked start playActivity
+            startActivity(Intent(this, playActivity::class.java))
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/memory_jogger/playActivity.kt
@@ -0,0 +1,12 @@
+package com.example.memory_jogger
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class playActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {        //when layout is created do this{
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.play_layout)                    //show
+    }
+}
